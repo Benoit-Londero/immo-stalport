8a60e031751bcbc981429df99503b51cef5010ac
Benito <you@example.com>
2024-02-22 22:48:18 +0100

    Correction section contact footer

--- templates-parts/contact.php
+++ templates-parts/contact.php
@@ -4,10 +4,13 @@
             $subtitle = get_field('sub_contact','options');
             $title = get_field('titre-contact','options');
             $form = get_field('shortcode_form','options');
+            $descr_Ctt = get_field('descr-contact','options');
 
         ?>
-        <p class="subtitle"><?php echo $subtitle;?></p>
-        <h2><?php echo $title;?></h2>
+        <?php if($subtitle):?><p class="subtitle"><?php echo $subtitle;?></p><?php endif;?>
+        <?php if($title):?><?php echo $title;?><?php endif;?>
+        
+        <?php if($descr_Ctt):?><?php echo $descr_Ctt;?><?php endif;?>
 
         <?php echo do_shortcode( $form );?>
     </div>
