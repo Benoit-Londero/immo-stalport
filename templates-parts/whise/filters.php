c3813b8758f880f5f21fb6d9539279e60ffc4d7e
Benito <you@example.com>
2024-02-24 16:00:18 +0100

    Ajout header contact

--- templates-parts/whise/filters.php
+++ templates-parts/whise/filters.php
@@ -126,107 +126,109 @@
   </div>
 </form>
 
-<div class="container-result">
+<?php if(!is_front_page() && !is_page(22)):?>
   <div class="container-result">
-    <div class="result">
-      <h2>Résultat pour :</h2>
+    <div class="container-result">
+      <div class="result">
+        <h2>Résultat pour :</h2>
 
-      <div class="result-display">
-        <?php if($filters):
-          foreach($fullType as $felem): ?>
-            <?php if(in_array($felem['id'],$listType)){
-              $queryParamsType = $_GET; // Copies the current query parameters
-              unset($queryParamsType['type']); // Change 'chambre' to the respective filter key
+        <div class="result-display">
+          <?php if($filters):
+            foreach($fullType as $felem): ?>
+              <?php if(in_array($felem['id'],$listType)){
+                $queryParamsType = $_GET; // Copies the current query parameters
+                unset($queryParamsType['type']); // Change 'chambre' to the respective filter key
 
-              $newQueryType = http_build_query($queryParamsType);
+                $newQueryType = http_build_query($queryParamsType);
 
-              echo "<a href='?{$newQueryType}'>".ucfirst($felem['name'])."</a>";
-            }
-          endforeach;
-          
-          if($_GET['chambre']):
-            $queryParamsChambre = $_GET; // Copies the current query parameters
-            unset($queryParamsChambre['chambre']); // Change 'chambre' to the respective filter key
-
-            $newQueryChambre = http_build_query($queryParamsChambre);
-            echo "<a href='?{$newQueryChambre}'>".$nbrChambre." Chambre(s)</a>";
-          endif;
-
-          if($_GET['region']):
-            foreach( $listRegions as $region):
-              if(in_array($region->id,$getRegion)):
-                $queryParamsRegions = $_GET; // Copies the current query parameters
-                unset($queryParamsRegions['region']); // Change 'chambre' to the respective filter key
-
-                $newQueryRegions = http_build_query($queryParamsRegions);
-
-                echo "<a href='?{$newQueryRegions}'>".substr($region->name,0,10)."...</a>";
-              endif;
+                echo "<a href='?{$newQueryType}'>".ucfirst($felem['name'])."</a>";
+              }
             endforeach;
-          endif;
+            
+            if($_GET['chambre']):
+              $queryParamsChambre = $_GET; // Copies the current query parameters
+              unset($queryParamsChambre['chambre']); // Change 'chambre' to the respective filter key
 
-          if($_GET['invest']):
-            $queryParamsInvest = $_GET; // Copies the current query parameters
-            unset($queryParamsInvest['invest']); // Change 'chambre' to the respective filter key
+              $newQueryChambre = http_build_query($queryParamsChambre);
+              echo "<a href='?{$newQueryChambre}'>".$nbrChambre." Chambre(s)</a>";
+            endif;
 
-            $newQueryInvest = http_build_query($queryParamsInvest);
-            echo "<a href='?{$newQueryInvest}'>Immeuble d'apport</a>";
-          endif;
+            if($_GET['region']):
+              foreach( $listRegions as $region):
+                if(in_array($region->id,$getRegion)):
+                  $queryParamsRegions = $_GET; // Copies the current query parameters
+                  unset($queryParamsRegions['region']); // Change 'chambre' to the respective filter key
 
-          if($_GET['localite']):
-            foreach($getLocalite as $rLocal):
-              $queryParamsLocalite = $_GET; // Copies the current query parameters
-              unset($queryParamsLocalite['localite']); // Change 'chambre' to the respective filter key
+                  $newQueryRegions = http_build_query($queryParamsRegions);
 
-              $newQueryLocalite = http_build_query($queryParamsLocalite);
-              echo "<a href='?{$newQueryLocalite}'>".$rLocal."</a>";
-            endforeach;
-          endif;
+                  echo "<a href='?{$newQueryRegions}'>".substr($region->name,0,10)."...</a>";
+                endif;
+              endforeach;
+            endif;
 
-          if($_GET['prixMinimum']):
-            $queryParamsPrixMin = $_GET; // Copies the current query parameters
-            unset($queryParamsPrixMin['prixMinimum']); // Change 'chambre' to the respective filter key
+            if($_GET['invest']):
+              $queryParamsInvest = $_GET; // Copies the current query parameters
+              unset($queryParamsInvest['invest']); // Change 'chambre' to the respective filter key
 
-            $newQueryPrixMin = http_build_query($queryParamsPrixMin);
-            echo "<a href='?{$newQueryPrixMin}'> Min. ".$prixMinimum."</a>";
-          endif;
+              $newQueryInvest = http_build_query($queryParamsInvest);
+              echo "<a href='?{$newQueryInvest}'>Immeuble d'apport</a>";
+            endif;
 
-          if($_GET['prixMaximum']):
-            $queryParamsPrixMax = $_GET; // Copies the current query parameters
-            unset($queryParamsPrixMax['prixMaximum']); // Change 'chambre' to the respective filter key
+            if($_GET['localite']):
+              foreach($getLocalite as $rLocal):
+                $queryParamsLocalite = $_GET; // Copies the current query parameters
+                unset($queryParamsLocalite['localite']); // Change 'chambre' to the respective filter key
 
-            $newQueryPrixMax = http_build_query($queryParamsPrixMax);
-            echo "<a href='?{$newQueryPrixMax}'>Max. ".$prixMaximum."</a>";
-          endif;  
-        endif;?>
+                $newQueryLocalite = http_build_query($queryParamsLocalite);
+                echo "<a href='?{$newQueryLocalite}'>".$rLocal."</a>";
+              endforeach;
+            endif;
+
+            if($_GET['prixMinimum']):
+              $queryParamsPrixMin = $_GET; // Copies the current query parameters
+              unset($queryParamsPrixMin['prixMinimum']); // Change 'chambre' to the respective filter key
+
+              $newQueryPrixMin = http_build_query($queryParamsPrixMin);
+              echo "<a href='?{$newQueryPrixMin}'> Min. ".$prixMinimum."</a>";
+            endif;
+
+            if($_GET['prixMaximum']):
+              $queryParamsPrixMax = $_GET; // Copies the current query parameters
+              unset($queryParamsPrixMax['prixMaximum']); // Change 'chambre' to the respective filter key
+
+              $newQueryPrixMax = http_build_query($queryParamsPrixMax);
+              echo "<a href='?{$newQueryPrixMax}'>Max. ".$prixMaximum."</a>";
+            endif;  
+          endif;?>
+        </div>
       </div>
     </div>
-  </div>
 
-  <form class="whise-filter-container" id="sort" action="" method="get">
-    <label for="sortBy"> Trier :</label>
-    <select name="order" id="sortBy" onchange="updateURL()">
-      <option <?php echo empty($_GET['order']) ? 'selected' : ''; ?>> Pertinences </option>
-      <option value="price_asc" <?php echo ($_GET['order'] === 'price_asc') ? 'selected' : ''; ?>>Prix croissant</option>
-      <option value="price_dsc" <?php echo ($_GET['order'] === 'price_dsc') ? 'selected' : ''; ?>>Prix décroissant</option>
-      <option value="communes" <?php echo ($_GET['order'] === 'communes') ? 'selected' : ''; ?>>Communes</option>
-      <option value="recents" <?php echo ($_GET['order'] === 'recents') ? 'selected' : ''; ?>>Les plus récents</option>
-    </select>
-  </form>
+    <form class="whise-filter-container" id="sort" action="" method="get">
+      <label for="sortBy"> Trier :</label>
+      <select name="order" id="sortBy" onchange="updateURL()">
+        <option <?php echo empty($_GET['order']) ? 'selected' : ''; ?>> Pertinences </option>
+        <option value="price_asc" <?php echo ($_GET['order'] === 'price_asc') ? 'selected' : ''; ?>>Prix croissant</option>
+        <option value="price_dsc" <?php echo ($_GET['order'] === 'price_dsc') ? 'selected' : ''; ?>>Prix décroissant</option>
+        <option value="communes" <?php echo ($_GET['order'] === 'communes') ? 'selected' : ''; ?>>Communes</option>
+        <option value="recents" <?php echo ($_GET['order'] === 'recents') ? 'selected' : ''; ?>>Les plus récents</option>
+      </select>
+    </form>
 
-  <script>
-    function updateURL() {
-      var form = document.getElementById('sort');
-      var order = form.querySelector('#sortBy').value;
+    <script>
+      function updateURL() {
+        var form = document.getElementById('sort');
+        var order = form.querySelector('#sortBy').value;
 
-      // Récupérer l'URL actuelle
-      var currentURL = new URL(window.location.href);
+        // Récupérer l'URL actuelle
+        var currentURL = new URL(window.location.href);
 
-      // Mettre à jour ou ajouter le paramètre 'order' à la chaîne de requête
-      currentURL.searchParams.set('order', order);
+        // Mettre à jour ou ajouter le paramètre 'order' à la chaîne de requête
+        currentURL.searchParams.set('order', order);
 
-      // Rediriger vers la nouvelle URL
-      window.location.href = currentURL.href;
-    }
-  </script>
+        // Rediriger vers la nouvelle URL
+        window.location.href = currentURL.href;
+      }
+    </script>
+<?php endif;?>
 </div>
