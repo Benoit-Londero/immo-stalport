2bd4a7ff4b7b2fc366b52261e52df16b05cea5bf
Benito <you@example.com>
2024-02-24 23:30:44 +0100

    contact

--- contact.php
+++ contact.php
@@ -3,12 +3,6 @@
 
 get_header();
 
-$titre = get_field('titre');
-$intro = get_field('intro');
-$form = get_field('formulaire');
-
-$bg_header = get_field('bg_header');
-
 if(!$bg_header):
     $bg_url = get_template_directory_uri(  ).'/assets/img/bg-default.jpg';
 else :
@@ -20,14 +14,21 @@ get_template_part( 'templates-parts/header-nav');?>
 
 <header id="header" style="background:url('<?php echo $bg_url;?>');">
 </header>
+<section id="contact">
+    <div class="container from-bottom">
+    <?php
+        $titre = get_field('titre');
+        $intro = get_field('intro');
+        $form = get_field('formulaire');
+        $bg_header = get_field('bg_header');
+    ?>
+    <?php if($title):?><?php echo $title;?><?php endif;?>
+    <span class="separator"></span>
+    <?php if($descr_Ctt):?><?php echo $descr_Ctt;?><?php endif;?>
 
-<section id="contact-content">
-    <div class="container">
-            <?php if($titre) : echo $titre;endif;?>
-            <?php if($form) : echo do_shortcode($form);endif;?>
     </div>
-</section>
-
-<?php get_template_part('templates-parts/contact');?>
+    <div class="container container-form">
+        <?php echo do_shortcode( $form );?>
+</div>
 
 <?php get_footer();
