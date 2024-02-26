1dcadf8cdac8db6094a6a35ce36103fd3ec59ae3
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-02-26 15:56:23 +0100

    correction slide / css cta

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -7,41 +7,41 @@ const swiper = new Swiper(".swiper", {
   speed: 3000,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
-  }
+    clickable: true,
+  },
 });
 
 const swiper_about = new Swiper(".swiper-about", {
   loop: false,
   autoplay: false,
-  cssMode : true,
-  slidesPerView : 1,
+  cssMode: true,
+  slidesPerView: 1,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-})
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 const swiper_mea = new Swiper(".swiper-mea", {
   loop: false,
   autoplay: false,
-  cssMode : true,
-  slidesPerView : 1,
+  cssMode: true,
+  slidesPerView: 1,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-})
+    nextEl: ".swiper-button-next-2",
+    prevEl: ".swiper-button-prev-2",
+  },
+});
 
 const swiper_estate = new Swiper(".swiper-estate", {
   spaceBetween: 10,
-  slidesPerView : 3,
+  slidesPerView: 3,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   breakpoints: {
     320: {
@@ -49,6 +49,6 @@ const swiper_estate = new Swiper(".swiper-estate", {
     },
     640: {
       slidesPerView: 3,
-    }
-  } 
-})
+    },
+  },
+});
